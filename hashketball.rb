c8b85aefb3ee834128f6b5a57c36ca92e532cd6b
Alexandra R <alexandra.ricardel@gmail.com>
2020-12-28 02:12:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,3 +130,38 @@ end
 def num_points_scored
   game_hash
 end
+
+def shoe_size
+end
+
+def team_colors
+  
+end
+
+def team_names
+  
+end
+
+def player_stats
+  
+end
+
+def big_shoe_rebounds
+  
+end
+
+def most_points_scored
+  
+end
+
+def winning_team
+  
+end
+
+def player_with_longest_name
+  
+end
+
+def long_name_steals_a_ton
+  
+end
